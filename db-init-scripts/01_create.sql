9a32b72b7c68f6e973074aaa13db4fac96836016
Giuseppe Zileni <giuseppe.zileni@gmail.com>
2023-01-06 19:45:35 +0100

    first commit

--- db-init-scripts/01_create.sql
+++ db-init-scripts/01_create.sql
@@ -1,3 +1,24 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6cc50c35ff2acc2699836c22bcc1705c2b97ffddbf416a9d9d4c3e7011e994e8
-size 963
+GRANT ALL PRIVILEGES ON DATABASE geodata TO geodata;
+CREATE SCHEMA IF NOT EXISTS gis;
+ALTER DATABASE geodata SET search_path=public,gis;
+GRANT ALL PRIVILEGES ON SCHEMA gis TO geodata;
+
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_raster;
+
+ALTER DATABASE postgres SET postgis.enable_outdb_rasters = true;
+ALTER DATABASE postgres SET postgis.gdal_enabled_drivers TO 'ENABLE_ALL';
+
+ALTER DATABASE geodata SET postgis.enable_outdb_rasters = true;
+ALTER DATABASE geodata SET postgis.gdal_enabled_drivers TO 'ENABLE_ALL';
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
--- db-init-scripts/02_dtm_tables.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:58762b76e688f10d1df3f0d8577c8fd05108de936cfa98ff9b60b2b929d60e63
-size 1808
